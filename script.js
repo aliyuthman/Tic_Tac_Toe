7b9c4998f2f50cdd6e89a93d463f5b783f7f3ab0
Aliyu Usman <aliyuusman64@gmail.com>
2022-02-13 14:28:44 +0100

    Bunches of if-statement added 🤣😂

--- script.js
+++ script.js
@@ -1,191 +1,97 @@
-const selectPlayer_X = [];
-const selectedPlayer_O = [];
+const playerXselection = [];
+const playerOselection = [];
 
-// if (selectPlayer_X.length === 1 && selectedPlayer_O.length === 2) {
 
-//     return;
-// }
+function xWinner(){
 
-// if (selectPlayer_X.length === 1 && selectedPlayer_O.length === 2) {
-
-//   selectedPlayer_O.pop("o");
-
-// }
-
-
-console.log(document.querySelector(".default-msg").style.display === "")
-
-
-function playerXchoice(){
-
-document.querySelector(".cells-container").addEventListener("click", event =>{
-
-
-
-
-});
-
-
-}
-
-
-
-function playerOchoice(){
-
-    document.querySelector(".cells-container").addEventListener("click", event =>{
-
-    
-
-      if (Array.from(event.target.classList).includes("one")){
-
-
-        document.getElementById("player-icons-1").src = "./SVG/large-o.svg";
-
-
-
-      }
-
-
-      if (Array.from(event.target.classList).includes("two")){
-
-        document.getElementById("player-icons-2").src = "./SVG/large-o.svg";
-
-      }
-
-
-      if (Array.from(event.target.classList).includes("three")){
-
-        document.getElementById("player-icons-3").src = "./SVG/large-o.svg";
-
-      }
-
-      if (Array.from(event.target.classList).includes("four")){
-
-        document.getElementById("player-icons-4").src = "./SVG/large-o.svg";
-
-      }
-
-      if (Array.from(event.target.classList).includes("five")){
-
-        document.getElementById("player-icons-5").src = "./SVG/large-o.svg";
-
-      }
-
-      if (Array.from(event.target.classList).includes("six")){
-
-        document.getElementById("player-icons-6").src = "./SVG/large-o.svg";
-
-      }
-
-
-      if (Array.from(event.target.classList).includes("seven")){
-
-        document.getElementById("player-icons-7").src = "./SVG/large-o.svg";
-
-      }
-
-
-      if (Array.from(event.target.classList).includes("eight")){
-
-        document.getElementById("player-icons-8").src = "./SVG/large-o.svg";
-
-      }
-
-
-      if (Array.from(event.target.classList).includes("nine")){
-
-        document.getElementById("player-icons-9").src = "./SVG/large-o.svg";
-
-      }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-        
-        });
-        
-
-}
-
-
-function playerXchoice(){
-
-  document.querySelector(".cells-container").addEventListener("click", event =>{
 
   
+  document.querySelector(".cells-container").classList.add("hidden");
+  document.querySelector(".x-win").classList.remove("hidden");
+  
 
-    if (Array.from(event.target.classList).includes("one")){
+
+
+}
+
+
+
+function oWinner(){
+
+  
+  document.querySelector(".cells-container").classList.add("hidden");
+  document.querySelector(".o-win").classList.remove("hidden");
+  
+
+
+}
+
+
+
+function playerXchoice() {
+
+  document.querySelector(".cells-container").addEventListener("click", event => {
+
+
+
+    if (Array.from(event.target.classList).includes("one")) {
 
 
       document.getElementById("player-icons-1").src = "./SVG/large-x.svg";
-
-
-
+      checkXwinner();
     }
 
 
-    if (Array.from(event.target.classList).includes("two")){
+    if (Array.from(event.target.classList).includes("two")) {
 
       document.getElementById("player-icons-2").src = "./SVG/large-x.svg";
-
+      checkXwinner();
     }
 
 
-    if (Array.from(event.target.classList).includes("three")){
+    if (Array.from(event.target.classList).includes("three")) {
 
       document.getElementById("player-icons-3").src = "./SVG/large-x.svg";
-
+      checkXwinner();
     }
 
-    if (Array.from(event.target.classList).includes("four")){
+    if (Array.from(event.target.classList).includes("four")) {
 
       document.getElementById("player-icons-4").src = "./SVG/large-x.svg";
-
+      checkXwinner();
     }
 
-    if (Array.from(event.target.classList).includes("five")){
+    if (Array.from(event.target.classList).includes("five")) {
 
       document.getElementById("player-icons-5").src = "./SVG/large-x.svg";
-
+      checkXwinner();
     }
 
-    if (Array.from(event.target.classList).includes("six")){
+    if (Array.from(event.target.classList).includes("six")) {
 
       document.getElementById("player-icons-6").src = "./SVG/large-x.svg";
-
+      checkXwinner();
     }
 
 
-    if (Array.from(event.target.classList).includes("seven")){
+    if (Array.from(event.target.classList).includes("seven")) {
 
       document.getElementById("player-icons-7").src = "./SVG/large-x.svg";
-
+      checkXwinner();
     }
 
 
-    if (Array.from(event.target.classList).includes("eight")){
+    if (Array.from(event.target.classList).includes("eight")) {
 
       document.getElementById("player-icons-8").src = "./SVG/large-x.svg";
-
+      checkXwinner();
     }
 
 
-    if (Array.from(event.target.classList).includes("nine")){
+    if (Array.from(event.target.classList).includes("nine")) {
 
       document.getElementById("player-icons-9").src = "./SVG/large-x.svg";
+      checkXwinner();
 
     }
 
@@ -194,77 +100,359 @@ function playerXchoice(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-      
-      
-      });
-      
-
-}
-
-function handlePlayerO() {
-
-  if(document.querySelector(".default-msg").style.visible){
-
-
-
-  }
-  document.querySelector(".default-msg").classList.add("hidden");
-  document.querySelector(".o-turn").classList.remove("hidden");
-  document.querySelector(".x-turn").classList.add("hidden");
-  document.querySelector(".x-score").classList.remove("bottom-border");
-
-
- 
+  });
 
 
 }
+
 
 function handlePlayerX() {
 
- 
-  document.querySelector(".o-turn").classList.add("hidden");
-  document.querySelector(".x-turn").classList.remove("hidden");
+  if (window.getComputedStyle(document.querySelector(".default-msg"), null).getPropertyValue("display") === "block") {
+
+    document.querySelector(".default-msg").classList.add("hidden");
+    document.querySelector(".o-turn").classList.add("hidden");
+    document.querySelector(".x-turn").classList.remove("hidden");
 
 
-
-}
-
-function buttonClicked(event) {
-  if (
-    event.target.classList.contains("x-score") ||
-    event.target.classList.contains("o-score")
-  ) {
-    switch (event.target.className) {
-      case "x-score":
-        handlePlayerX();
-        
-
-
-        break;
-
-      case "o-score":
-        handlePlayerO();
-        
-
-        break;
-    }
   }
+
+
+  document.querySelector(".x-turn").classList.remove("hidden");
+  document.querySelector(".o-turn").classList.add("hidden");
+
 }
 
+function checkOwinner() {
+  if (playerOselection.length === 3 || playerOselection.length === 4 || playerOselection.length === 5) {
+
+
+    if (
+      document.getElementById("player-icons-1").src.includes("large-o") &&
+      document.getElementById("player-icons-2").src.includes("large-o") &&
+      document.getElementById("player-icons-3").src.includes("large-o")) {
+
+      oWinner();
+
+    }
+
+    if (
+      document.getElementById("player-icons-4").src.includes("large-o") &&
+      document.getElementById("player-icons-5").src.includes("large-o") &&
+      document.getElementById("player-icons-6").src.includes("large-o")) {
+
+        oWinner();
+
+    }
+
+    if (
+      document.getElementById("player-icons-7").src.includes("large-o") &&
+      document.getElementById("player-icons-8").src.includes("large-o") &&
+      document.getElementById("player-icons-9").src.includes("large-o")) {
+
+        oWinner();
+    }
+
+
+    if (
+
+      document.getElementById("player-icons-1").src.includes("large-o") &&
+      document.getElementById("player-icons-5").src.includes("large-o") &&
+      document.getElementById("player-icons-9").src.includes("large-o")) {
+
+        oWinner();
+
+    }
+
+    if (
+      document.getElementById("player-icons-3").src.includes("large-o") &&
+      document.getElementById("player-icons-5").src.includes("large-o") &&
+      document.getElementById("player-icons-7").src.includes("large-o")) {
+
+
+        oWinner();
+
+    }
+
+
+    if (
+      document.getElementById("player-icons-1").src.includes("large-o") &&
+      document.getElementById("player-icons-4").src.includes("large-o") &&
+      document.getElementById("player-icons-7").src.includes("large-o")) {
+
+
+        oWinner();
+
+    }
+
+
+    if (
+      document.getElementById("player-icons-2").src.includes("large-o") &&
+      document.getElementById("player-icons-5").src.includes("large-o") &&
+      document.getElementById("player-icons-8").src.includes("large-o")) {
+
+
+        oWinner();
+
+    }
+
+
+    if (
+      document.getElementById("player-icons-3").src.includes("large-o") &&
+      document.getElementById("player-icons-6").src.includes("large-o") &&
+      document.getElementById("player-icons-9").src.includes("large-o")) {
+
+
+        oWinner();
+
+    }
+
+
+
+  }
+
+}
+
+function checkXwinner() {
+  if (playerXselection.length === 3 || playerXselection.length === 4 || playerXselection.length === 5) {
+
+
+    if (
+      document.getElementById("player-icons-1").src.includes("large-x") &&
+      document.getElementById("player-icons-2").src.includes("large-x") &&
+      document.getElementById("player-icons-3").src.includes("large-x")) {
+
+     xWinner();
+
+    }
+
+    if (
+      document.getElementById("player-icons-4").src.includes("large-x") &&
+      document.getElementById("player-icons-5").src.includes("large-x") &&
+      document.getElementById("player-icons-6").src.includes("large-x")) {
+
+       xWinner();
+
+    }
+
+    if (
+      document.getElementById("player-icons-7").src.includes("large-x") &&
+      document.getElementById("player-icons-8").src.includes("large-x") &&
+      document.getElementById("player-icons-9").src.includes("large-x")) {
+
+       xWinner();
+    }
+
+
+    if (
+
+      document.getElementById("player-icons-1").src.includes("large-x") &&
+      document.getElementById("player-icons-5").src.includes("large-x") &&
+      document.getElementById("player-icons-9").src.includes("large-x")) {
+
+       xWinner();
+
+    }
+
+    if (
+      document.getElementById("player-icons-3").src.includes("large-x") &&
+      document.getElementById("player-icons-5").src.includes("large-x") &&
+      document.getElementById("player-icons-7").src.includes("large-x")) {
+
+
+       xWinner();
+
+    }
+
+
+    if (
+      document.getElementById("player-icons-1").src.includes("large-x") &&
+      document.getElementById("player-icons-4").src.includes("large-x") &&
+      document.getElementById("player-icons-7").src.includes("large-x")) {
+
+
+     xWinner();
+
+    }
+
+
+    if (
+      document.getElementById("player-icons-2").src.includes("large-x") &&
+      document.getElementById("player-icons-5").src.includes("large-x") &&
+      document.getElementById("player-icons-8").src.includes("large-x")) {
+
+
+       xWinner();
+
+    }
+
+
+    if (
+      document.getElementById("player-icons-3").src.includes("large-x") &&
+      document.getElementById("player-icons-6").src.includes("large-x") &&
+      document.getElementById("player-icons-9").src.includes("large-x")) {
+
+
+       xWinner();
+
+    }
+
+
+
+  }
+
+}
+
+//player o choosing cell
+
+function playerOchoice() {
+
+  document.querySelector(".cells-container").addEventListener("click", event => {
+
+
+    if (Array.from(event.target.classList).includes("one")) {
+
+
+      document.getElementById("player-icons-1").src = "./SVG/large-o.svg";
+      checkOwinner();
+
+    }
+
+
+    if (Array.from(event.target.classList).includes("two")) {
+
+      document.getElementById("player-icons-2").src = "./SVG/large-o.svg";
+      checkOwinner();
+
+    }
+
+
+    if (Array.from(event.target.classList).includes("three")) {
+
+      document.getElementById("player-icons-3").src = "./SVG/large-o.svg";
+      checkOwinner();
+    }
+
+    if (Array.from(event.target.classList).includes("four")) {
+
+      document.getElementById("player-icons-4").src = "./SVG/large-o.svg";
+      checkOwinner();
+    }
+
+    if (Array.from(event.target.classList).includes("five")) {
+
+      document.getElementById("player-icons-5").src = "./SVG/large-o.svg";
+      checkOwinner();
+    }
+
+    if (Array.from(event.target.classList).includes("six")) {
+
+      document.getElementById("player-icons-6").src = "./SVG/large-o.svg";
+      checkOwinner();
+    }
+
+
+    if (Array.from(event.target.classList).includes("seven")) {
+
+      document.getElementById("player-icons-7").src = "./SVG/large-o.svg";
+      checkOwinner();
+    }
+
+
+    if (Array.from(event.target.classList).includes("eight")) {
+
+      document.getElementById("player-icons-8").src = "./SVG/large-o.svg";
+      checkOwinner();
+    }
+
+
+    if (Array.from(event.target.classList).includes("nine")) {
+
+      document.getElementById("player-icons-9").src = "./SVG/large-o.svg";
+      checkOwinner();
+    }
+
+
+
+
+
+  });
+
+
+}
+
+
+//handling player O
+function handlePlayerO() {
+
+  if (window.getComputedStyle(document.querySelector(".default-msg")).getPropertyValue("display") === "block") {
+
+    document.querySelector(".default-msg").classList.add("hidden");
+    document.querySelector(".o-turn").classList.remove("hidden");
+
+  } else {
+
+    document.querySelector(".x-turn").classList.add("hidden");
+    document.querySelector(".o-turn").classList.remove("hidden");
+  }
+
+}
+
+
+//The program begins from here
 function init() {
   document.querySelector(".score-board").addEventListener("click", (event) => {
-    buttonClicked(event);
+
+
+    if (event.target.tagName === "DIV") {
+
+      if (event.target.className === "x-score") {
+
+        handlePlayerX();
+        playerXselection.push("X");
+
+        if (window.getComputedStyle(document.querySelector(".x-turn")).getPropertyValue("display") === "block") {
+
+          playerXchoice();
+          console.log(playerXselection);
+
+        }
+
+      }
+
+      if (event.target.className === "o-score") {
+
+        handlePlayerO();
+        playerOselection.push("O");
+
+        if (window.getComputedStyle(document.querySelector(".o-turn")).getPropertyValue("display") === "block") {
+
+          playerOchoice();
+          console.log(playerOselection);
+
+        }
+
+      }
+
+
+    }
+
   });
+
+  
+
+document.querySelector(".btn-restart").addEventListener('click', event =>{
+
+
+window.location.reload();
+ 
+ 
+ })
+ 
+
+
 }
 
+
 init();
+
